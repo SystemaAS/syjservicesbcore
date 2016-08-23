08d9fa1b3a5d65d79627724d4215cd132ae563e1
Oscar de la Torre <oscar@systema.no>
2016-08-23 21:38:57 +0200

    update

---
+++ src/no/systema/jservices/bcore/z/maintenance/model/dao/services/sadimport/StandiDaoServicesImpl.java
@@ -0,0 +1,164 @@
+package no.systema.jservices.bcore.z.maintenance.model.dao.services.sadimport;
+import java.io.Writer;
+import java.util.*;
+
+import org.apache.log4j.Logger;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import no.systema.jservices.bcore.z.maintenance.model.dao.mapper.sadimport.StandiMapper;
+import no.systema.jservices.bcore.z.maintenance.model.dao.entities.sadimport.StandiDao;
+import no.systema.main.util.DbErrorMessageManager;
+
+/**
+ * 
+ * @author oscardelatorre
+ * @date Aug 23, 2016
+ * 
+ */
+public class StandiDaoServicesImpl implements StandiDaoServices {
+	private static Logger logger = Logger.getLogger(StandiDaoServicesImpl.class.getName());
+	private DbErrorMessageManager dbErrorMessageMgr = new DbErrorMessageManager();
+	
+	/**
+	 * 
+	 */
+	public List getList(StringBuffer errorStackTrace){
+		List<StandiDao> retval = new ArrayList<StandiDao>();
+		
+		try{
+			StringBuffer sql = new StringBuffer();
+			sql.append(" select siavd, sitdn ");
+			sql.append(" from standi ");
+			
+			retval = this.jdbcTemplate.query( sql.toString(), new StandiMapper());
+			
+		}catch(Exception e){
+			Writer writer = this.dbErrorMessageMgr.getPrintWriter(e);
+			logger.info(writer.toString());
+			//Chop the message to comply to JSON-validation
+			errorStackTrace.append(this.dbErrorMessageMgr.getJsonValidDbException(writer));
+			retval = null;
+		}
+		return retval;
+	}
+	/**
+	 * 
+	 */
+	public List findById (String id, StringBuffer errorStackTrace ){
+		List<StandiDao> retval = new ArrayList<StandiDao>();
+		try{
+			StringBuffer sql = new StringBuffer();
+			
+			sql.append(" select siavd, sitdn ");
+			sql.append(" from standi ");
+			//WHERE
+			sql.append(" where siavd = ?  ");
+			
+			retval = this.jdbcTemplate.query( sql.toString(), new Object[] { id }, new StandiMapper());
+			
+		}catch(Exception e){
+			Writer writer = this.dbErrorMessageMgr.getPrintWriter(e);
+			logger.info(writer.toString());
+			//Chop the message to comply to JSON-validation
+			errorStackTrace.append(this.dbErrorMessageMgr.getJsonValidDbException(writer));
+		}
+		return retval;
+	}
+	
+	/**
+	 * 
+	 * @param dao
+	 * @param errorStackTrace
+	 * @return
+	 */
+	
+	public int insert(Object daoObj, StringBuffer errorStackTrace){
+		int retval = 0;
+		try{
+			/* TODO
+			KodtaDao dao = (KodtaDao)daoObj;
+			StringBuffer sql = new StringBuffer();
+			//DEBUG --> logger.info("mydebug");
+			sql.append(" INSERT INTO firm ( koaavd, navsg ) ");
+			sql.append(" VALUES ( ?, ? ) ");
+			//params
+			retval = this.jdbcTemplate.update( sql.toString(), new Object[] { dao.getKoaavd(), dao.getNavsg() } );
+			*/
+		}catch(Exception e){
+			Writer writer = this.dbErrorMessageMgr.getPrintWriter(e);
+			logger.info(writer.toString());
+			//Chop the message to comply to JSON-validation
+			errorStackTrace.append(this.dbErrorMessageMgr.getJsonValidDbException(writer));
+			retval = -1;
+		}
+		
+		return retval;
+	}
+	
+	
+	/**
+	 * UPDATE
+	 */
+	public int update(Object daoObj, StringBuffer errorStackTrace){
+		int retval = 0;
+		
+		try{
+			/* TODO
+			KodtaDao dao = (KodtaDao)daoObj;
+			StringBuffer sql = new StringBuffer();
+			//DEBUG --> logger.info("mydebug");
+			sql.append(" UPDATE firm SET xxx = ? ");
+			sql.append(" WHERE xxx = ? ");
+			//params
+			retval = this.jdbcTemplate.update( sql.toString(), new Object[] { dao.getXXX(), 
+				//WHERE
+				dao.getKoaavd() } );
+			*/
+		}catch(Exception e){
+			Writer writer = this.dbErrorMessageMgr.getPrintWriter(e);
+			logger.info(writer.toString());
+			//Chop the message to comply to JSON-validation
+			errorStackTrace.append(this.dbErrorMessageMgr.getJsonValidDbException(writer));
+			retval = -1;
+		}
+		
+		return retval;
+	}
+	
+	/**
+	 * DELETE
+	 */
+	
+	public int delete(Object daoObj, StringBuffer errorStackTrace){
+		int retval = 0;
+		try{
+			/* TODO
+			KodtaDao dao = (KodtaDao)daoObj;
+			StringBuffer sql = new StringBuffer();
+			//DEBUG --> logger.info("mydebug");
+			sql.append(" DELETE from navavd ");
+			sql.append(" WHERE koaavd = ? ");
+			//params
+			retval = this.jdbcTemplate.update( sql.toString(), new Object[] { dao.getKoaavd() } );
+			*/
+		}catch(Exception e){
+			Writer writer = this.dbErrorMessageMgr.getPrintWriter(e);
+			logger.info(writer.toString());
+			//Chop the message to comply to JSON-validation
+			errorStackTrace.append(this.dbErrorMessageMgr.getJsonValidDbException(writer));
+			retval = -1;
+		}
+		
+		return retval;
+	}
+	
+	
+	/**                                                                                                  
+	 * Wires jdbcTemplate                                                                                
+	 *                                                                                                   
+	 */                                                                                                  
+	private JdbcTemplate jdbcTemplate = null;                                                            
+	public void setJdbcTemplate( JdbcTemplate jdbcTemplate) {this.jdbcTemplate = jdbcTemplate;}          
+	public JdbcTemplate getJdbcTemplate() {return this.jdbcTemplate;}                                    
+
+}
