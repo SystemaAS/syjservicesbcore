205c9dcba197263fb2f50d9eb0f4e227059c4b9f
Oscar de la Torre <oscar@systema.no>
2017-05-09 16:32:02 +0200

    Update

--- src/no/systema/jservices/bcore/z/maintenance/controller/BcoreMaintResponseOutputterController_SYPARF2.java
+++ src/no/systema/jservices/bcore/z/maintenance/controller/BcoreMaintResponseOutputterController_SYPARF2.java
@@ -20,38 +20,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import no.systema.jservices.bcore.z.maintenance.controller.rules.SYPARF_U;
+import no.systema.jservices.bcore.z.maintenance.controller.rules.SYPARF2_U;
 import no.systema.jservices.bcore.z.maintenance.model.dao.entities.KofastDao;
 import no.systema.jservices.bcore.z.maintenance.model.dao.services.KofastDaoServices;
 import no.systema.jservices.common.dao.SyparfDao;
 import no.systema.jservices.common.dao.services.FratxtDaoService;
-import no.systema.jservices.common.dao.services.SyparfDaoService;
+import no.systema.jservices.common.dao.services.Syparf2DaoService;
 import no.systema.jservices.common.dto.SyparfDto;
 import no.systema.jservices.common.json.JsonResponseWriter2;
 import no.systema.jservices.common.values.FasteKoder;
 import no.systema.jservices.model.dao.services.BridfDaoServices;
 
 @Controller
-public class BcoreMaintResponseOutputterController_SYPARF {
-	private static final Logger logger = Logger.getLogger(BcoreMaintResponseOutputterController_SYPARF.class.getName());
+public class BcoreMaintResponseOutputterController_SYPARF2 {
+	private static final Logger logger = Logger.getLogger(BcoreMaintResponseOutputterController_SYPARF2.class.getName());
 
 	/**
-	 * FreeForm Source: File: SYPARF
+	 * FreeForm Source: File: SYPARF, based on syuser as key (USER)
 	 * 
 	 * @return
 	 * @Example SELECT
-	 *          specific:http://gw.systema.no:8080/syjservicesbcore/syjsSYPARF.do?user=OSCAR&sykunr=1&syrecn=15
+	 *          specific:http://gw.systema.no:8080/syjservicesbcore/syjsSYPARF2.do?user=OSCAR&syuser=1&syrecn=15
 	 * @Example SELECT list:
-	 *          http://gw.systema.no:8080/syjservicesbcore/syjsSYPARF.do?user=OSCAR&sykunr=1
+	 *          http://gw.systema.no:8080/syjservicesbcore/syjsSYPARF2.do?user=OSCAR&syuser=1
 	 * 
 	 */
-	@RequestMapping(value = "syjsSYPARF.do", method = { RequestMethod.GET, RequestMethod.POST })
+	@RequestMapping(value = "syjsSYPARF2.do", method = { RequestMethod.GET, RequestMethod.POST })
 	@ResponseBody
-	public String doSyparf(HttpSession session, HttpServletRequest request) {
+	public String doSyparf2(HttpSession session, HttpServletRequest request) {
 		JsonResponseWriter2<SyparfDto> jsonWriter = new JsonResponseWriter2<SyparfDto>();
 		StringBuffer sb = new StringBuffer();
 		List<SyparfDto> syparfDtoList = new ArrayList<SyparfDto>();
-		String sykunr = request.getParameter("sykunr");
+		String syuser = request.getParameter("syuser");
 		String syrecn = request.getParameter("syrecn");
 
 		try {
@@ -61,13 +61,16 @@ public class BcoreMaintResponseOutputterController_SYPARF {
 			String errMsg = "";
 			String status = "ok";
 			StringBuffer dbErrorStackTrace = new StringBuffer();
-			if ((userName != null && !"".equals(userName)) && (sykunr != null && !"".equals(sykunr))) {
+			if (userName != null && !"".equals(userName) && (syuser != null && !"".equals(syuser)) ) {
 				if (syrecn != null && !"".equals(syrecn)) {
-					SyparfDto dto = fetchRecord(sykunr, syrecn);
+					logger.info("A");
+					SyparfDto dto = fetchRecord(syuser, syrecn);
 					syparfDtoList.add(dto);
 				} else {
-					syparfDtoList = fetchList(sykunr);
+					logger.info("B");
+					syparfDtoList = fetchList(syuser);
 				}
+				
 				if (syparfDtoList != null) {
 					sb.append(jsonWriter.setJsonResult_Common_GetList(userName, syparfDtoList));
 				} else {
@@ -107,23 +110,25 @@ public class BcoreMaintResponseOutputterController_SYPARF {
 	 * Update Database DML operations File: FRATXT
 	 * 
 	 * @Example UPDATE:
-	 *          http://gw.systema.no:8080/syjservicesbcore/syjsSYPARF_U.do?user=OSCAR&sykunr=24&syrecn=59&sysort=15&syvrdn=1,5&syvrda=alfaupdate&mode=U/A/D
+	 *          http://gw.systema.no:8080/syjservicesbcore/syjsSYPARF2_U.do?user=OSCAR&syuser=BIRGIT&syrecn=59&sysort=15&syvrdn=1,5&syvrda=alfaupdate&mode=U/A/D
 	 *
 	 * @param session
 	 * @param request
 	 * @return
 	 * 
 	 */
-	@RequestMapping(value = "syjsSYPARF_U.do", method = { RequestMethod.GET, RequestMethod.POST })
+	@RequestMapping(value = "syjsSYPARF2_U.do", method = { RequestMethod.GET, RequestMethod.POST })
 	@ResponseBody
-	public String syjsSYPARF_U(HttpSession session, HttpServletRequest request) {
+	public String syjsSYPARF2_U(HttpSession session, HttpServletRequest request) {
 		JsonResponseWriter2<SyparfDao> jsonWriter = new JsonResponseWriter2<SyparfDao>();
 		StringBuffer sb = new StringBuffer();
-
+		
 		try {
-			logger.info("Inside syjsSYPARF_U.do");
+			logger.info("Inside syjsSYPARF2_U.do");
 			String user = request.getParameter("user");
 			String mode = request.getParameter("mode");
+			String syuser = request.getParameter("syuser");
+			
 			// Check ALWAYS user in BRIDF
 			String userName = this.bridfDaoServices.findNameById(user);
 			String errMsg = "";
@@ -134,21 +139,23 @@ public class BcoreMaintResponseOutputterController_SYPARF {
 			SyparfDao resultDao = new SyparfDao();
 			ServletRequestDataBinder binder = new ServletRequestDataBinder(dto);
 			binder.bind(request);
-
-			SYPARF_U rulerLord = new SYPARF_U(request, kofastDaoServices, sb, dbErrorStackTrace);
+			//Special edition for USER
+			resultDao.setKeys(syuser);
+			//
+			SYPARF2_U rulerLord = new SYPARF2_U(request, kofastDaoServices, sb, dbErrorStackTrace);
 			
 			if (userName != null && !"".equals(userName)) {
 				if ("D".equals(mode)) {
 					if (rulerLord.isValidInputForDelete(dto, userName, mode)) {
-						syparfDaoService.delete(dto);
+						syparf2DaoService.delete(dto);
 					}
 				} else {
 					if (rulerLord.isValidInput(dto, userName, mode)) {
 						rulerLord.updateNumericFieldsIfNull(dto);
 						if ("A".equals(mode)) {
-							resultDao = syparfDaoService.create(dto);
+							resultDao = syparf2DaoService.create(dto);
 						} else if ("U".equals(mode)) {
-							resultDao = syparfDaoService.update(dto);
+							resultDao = syparf2DaoService.update(dto);
 						}
 					} 
 				}
@@ -185,16 +192,16 @@ public class BcoreMaintResponseOutputterController_SYPARF {
 
 	
 	private SyparfDto fetchRecord(String sykunr, String syrecn) {
-		SyparfDao dao = (SyparfDao) syparfDaoService.find(sykunr, syrecn);
+		SyparfDao dao = (SyparfDao) syparf2DaoService.find(sykunr, syrecn);
 		SyparfDto dto = getDto(dao);
 
 		return dto;
 	}
-	private List<SyparfDto> fetchList(String sykunr) {
+	private List<SyparfDto> fetchList(String syuser) {
 		List<SyparfDao> syparfDaoList = new ArrayList<SyparfDao>(); // from DaoServcie
 		List<SyparfDto> syparfDtoList = new ArrayList<SyparfDto>(); // dto to GUI
 		SyparfDto syparfDto = null; // dto to GUI
-		syparfDaoList = (List<SyparfDao>) syparfDaoService.findAll(sykunr);
+		syparfDaoList = (List<SyparfDao>) syparf2DaoService.findAll(syuser);
 		for (SyparfDao syparfDao : syparfDaoList) {
 			syparfDto = getDto(syparfDao);
 			syparfDtoList.add(syparfDto);
@@ -204,13 +211,13 @@ public class BcoreMaintResponseOutputterController_SYPARF {
 	}
 	private SyparfDto getDto(SyparfDao dao) {
 		SyparfDto dto = new SyparfDto();
+		dto.setSyuser(dao.getSyuser());
+		dto.setSyrecn(dao.getSyrecn());
 		dto.setSykunr(dao.getSykunr());
 		dto.setSyavd(dao.getSyavd());
 		dto.setSypaid(dao.getSypaid());
 		dto.setSypaidDesc(getSytpaidDesc(dao.getSypaid()));
-		dto.setSyrecn(dao.getSyrecn());
 		dto.setSysort(dao.getSysort());
-		dto.setSyuser(dao.getSyuser());
 		dto.setSyvrda(dao.getSyvrda());
 		dto.setSyvrdn(dao.getSyvrdn());
 		return dto;
@@ -243,18 +250,12 @@ public class BcoreMaintResponseOutputterController_SYPARF {
 		return this.bridfDaoServices;
 	}
 
-	@Qualifier("syparfDaoService")
-	private SyparfDaoService syparfDaoService;
-
+	@Qualifier("syparf2DaoService")
+	private Syparf2DaoService syparf2DaoService;
 	@Autowired
 	@Required
-	public void setSyparfDaoService(SyparfDaoService value) {
-		this.syparfDaoService = value;
-	}
-
-	public SyparfDaoService getSyparfDaoService() {
-		return this.syparfDaoService;
-	}
+	public void setSyparf2DaoService(Syparf2DaoService value) { this.syparf2DaoService = value; }
+	public Syparf2DaoService getSyparf2DaoService() { return this.syparf2DaoService; }
 
 	@Qualifier("kofastDaoServices")
 	private KofastDaoServices kofastDaoServices;
---
+++ src/no/systema/jservices/bcore/z/maintenance/controller/rules/SYPARF2_U.java
@@ -0,0 +1,110 @@
+package no.systema.jservices.bcore.z.maintenance.controller.rules;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+
+import no.systema.jservices.bcore.z.maintenance.model.dao.entities.KofastDao;
+import no.systema.jservices.bcore.z.maintenance.model.dao.services.KofastDaoServices;
+import no.systema.jservices.common.dto.SyparfDto;
+import no.systema.jservices.common.json.JsonResponseWriter2;
+import no.systema.jservices.common.values.FasteKoder;
+import no.systema.main.util.MessageSourceHelper;
+
+/**
+ * 
+ * @author oscardelatorre
+ * @date May 9, 2017
+ */
+public class SYPARF2_U {
+	private static Logger logger = Logger.getLogger(SYPARF2_U.class.getName());
+	private JsonResponseWriter2<SyparfDto> jsonWriter = new JsonResponseWriter2<SyparfDto>();
+	private MessageSourceHelper messageSourceHelper = null;
+	private KofastDaoServices kofastDaoServices = null;
+	
+	private StringBuffer errors = null;
+	private StringBuffer dbErrors = null;
+
+	public SYPARF2_U(HttpServletRequest request, KofastDaoServices kofastDaoServices, StringBuffer sb, StringBuffer dbErrorStackTrace) {
+		messageSourceHelper = new MessageSourceHelper(request);
+		this.kofastDaoServices = kofastDaoServices;
+		this.errors = sb;
+		this.dbErrors = dbErrorStackTrace;
+	}
+
+	public boolean isValidInput(SyparfDto dto, String user, String mode) {
+		boolean retval = true;
+		if ((user != null && !"".equals(user)) && (mode != null && !"".equals(mode))) {
+			// check dto
+			if ( (dto.getSypaid() != null && !"".equals(dto.getSypaid()) ) 
+				&& (dto.getSyuser() != null && !"".equals(dto.getSyuser()) )  	
+				&& (dto.getSyvrdn() != null && !"".equals(dto.getSyvrdn()) )  
+				&& (dto.getSyvrda() != null && !"".equals(dto.getSyvrda()) ))  {
+				
+				if ("U".equals(mode) && (dto.getSyrecn() == null && "".equals(dto.getSyrecn())) ) {
+					errors.append(jsonWriter.setJsonSimpleErrorResult(user,
+							messageSourceHelper.getMessage("systema.bcore.saksbehandlere.user.error.mandatory", null), "error", dbErrors));
+					retval = false;
+				}
+				
+				if (!existInKofast(dto.getSypaid())) {
+					errors.append(jsonWriter.setJsonSimpleErrorResult(user,
+							messageSourceHelper.getMessage("systema.bcore.kunderegister.params.error.sypaid", new Object[] { dto.getSypaid() }), "error",
+							dbErrors));
+					retval = false;
+				}
+				
+			} else {
+				retval = false;
+			}
+		} else {
+			retval = false;
+		}
+
+		return retval;
+	}
+
+
+
+	public boolean isValidInputForDelete(SyparfDto dto, String user, String mode) {
+		boolean retval = true;
+		if ((user != null && !"".equals(user)) && (mode != null && !"".equals(mode))) {
+			// check dto
+			if ( (dto.getSyuser() != null && !"".equals(dto.getSyuser()) ) 
+					&& (dto.getSyrecn() != null && !"".equals(dto.getSyrecn()) )) { 
+				// OK
+			} else {
+				retval = false;
+			}
+		} else {
+			retval = false;
+		}
+		return retval;
+	}
+
+	private boolean existInKofast(String value) {
+		List<KofastDao> kofastList = kofastDaoServices.findById(FasteKoder.SYPAR, value, dbErrors);
+		if (kofastList != null && kofastList.size() == 1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void updateNumericFieldsIfNull(SyparfDto dto) {
+		String ZERO = "0";
+		if(dto.getSysort()==null || "".equals(dto.getSysort())){
+			dto.setSysort(ZERO);
+		}
+		if(dto.getSyavd()==null || "".equals(dto.getSyavd())){
+			dto.setSyavd(ZERO);
+		}		
+		if(dto.getSyvrdn()==null || "".equals(dto.getSyvrdn())){
+			dto.setSyvrdn(ZERO);
+		}
+	}
+
+	
+}
