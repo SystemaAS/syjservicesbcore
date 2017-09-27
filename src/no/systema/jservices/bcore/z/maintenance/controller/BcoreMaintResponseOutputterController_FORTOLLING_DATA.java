770b7ff6f8879f0092dde609071167ab591c4c0d
Fredrik Möller <fredrik@systema.no>
2017-09-27 10:14:06 +0200

    Added FORTOLLING_DATA

---
+++ src/no/systema/jservices/bcore/z/maintenance/controller/BcoreMaintResponseOutputterController_FORTOLLING_DATA.java
@@ -0,0 +1,114 @@
+package no.systema.jservices.bcore.z.maintenance.controller;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import no.systema.jservices.common.dao.services.FortollingDaoService;
+import no.systema.jservices.common.dto.FortollingDto;
+import no.systema.jservices.common.json.JsonResponseWriter2;
+import no.systema.jservices.common.util.StringUtils;
+import no.systema.jservices.model.dao.services.BridfDaoServices;
+
+@Controller
+public class BcoreMaintResponseOutputterController_FORTOLLING_DATA {
+	private static final Logger logger = Logger.getLogger(BcoreMaintResponseOutputterController_FORTOLLING_DATA.class.getName());
+
+	/**
+	 * File: SADH (fortolloing noreg)
+	 * 
+	 * @Example SELECT
+	 *          http://gw.systema.no:8080/syjservicesbcore/syjsFORTOLLING_DB.do?user=OSCAR&year=2016
+	 * 
+	 */
+	@RequestMapping(value = "syjsFORTOLLING_DB.do", method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public String doFortollingReportDashboard(HttpSession session, HttpServletRequest request) {
+		JsonResponseWriter2<FortollingDto> jsonWriter = new JsonResponseWriter2<FortollingDto>();
+		StringBuffer sb = new StringBuffer();
+		List<FortollingDto> fortollingDtoList = null;
+
+		logger.info("inside syjsFORTOLLING_DB.do");
+
+		try {
+			String user = request.getParameter("user");
+			String year = request.getParameter("year");
+			String userName = this.bridfDaoServices.findNameById(user);
+			String errMsg = "";
+			String status = "ok";
+			StringBuffer dbErrorStackTrace = new StringBuffer();
+
+			if (StringUtils.hasValue(userName)) {
+				logger.info("Retrieving data...");
+				fortollingDtoList = fortollingDaoService.getStats(Integer.valueOf(year));
+				if (fortollingDtoList != null) {
+					logger.info("fortollingDtoList.size()=" + fortollingDtoList.size());
+					logger.info("appendar till sb.");
+					sb.append(jsonWriter.setJsonResult_Common_GetList(userName, fortollingDtoList));
+					logger.info("appendat till sb!");
+				} else {
+					errMsg = "ERROR on SELECT: Can not find FortollingDto list";
+					status = "error";
+					logger.info(status + errMsg);
+					sb.append(errMsg);
+				}
+			} else {
+				errMsg = "ERROR on SELECT";
+				status = "error";
+				dbErrorStackTrace.append(" request input parameters are invalid: <user>");
+				sb.append(userName + errMsg + status + dbErrorStackTrace);
+			}
+		} catch (Exception e) {
+			logger.info("Error :", e);
+			Writer writer = new StringWriter();
+			PrintWriter printWriter = new PrintWriter(writer);
+			e.printStackTrace(printWriter);
+			return "ERROR [JsonResponseOutputterController]" + writer.toString();
+		}
+
+		session.invalidate();
+		logger.info("About to return...");
+		return sb.toString();
+
+	}
+
+	@Qualifier("bridfDaoServices")
+	private BridfDaoServices bridfDaoServices;
+
+	@Autowired
+	@Required
+	public void setBridfDaoServices(BridfDaoServices value) {
+		this.bridfDaoServices = value;
+	}
+
+	public BridfDaoServices getBridfDaoServices() {
+		return this.bridfDaoServices;
+	}
+
+	@Qualifier("fortollingDaoService")
+	private FortollingDaoService fortollingDaoService;
+
+	@Autowired
+	@Required
+	public void setFortollingDaoService(FortollingDaoService value) {
+		this.fortollingDaoService = value;
+	}
+
+	public FortollingDaoService getFortollingDaoService() {
+		return this.fortollingDaoService;
+	}
+
+}
