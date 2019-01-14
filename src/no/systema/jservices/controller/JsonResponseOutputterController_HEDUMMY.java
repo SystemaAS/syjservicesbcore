fe09a58f8cdff97f8707689b79b079a2d63d96a3
Oscar de la Torre <oscar@systema.no>
2019-01-14 17:49:14 +0100

    Update

--- src/no/systema/jservices/controller/JsonResponseOutputterController_HEDUMMY.java
+++ src/no/systema/jservices/controller/JsonResponseOutputterController_HEDUMMY.java
@@ -233,6 +233,93 @@ public class JsonResponseOutputterController_HEDUMMY {
 		return sb.toString();
 	}
 	
+	/**
+	 *
+	 * 	 File: 		SYSPEDF.HEDUMMY with MEMBER: URD (AIR EXPORT) 
+	 * 	 PGM:		?		
+	 * 	 Member: 	URD - Maintenance - SELECT LIST or SELECT SPECIFIC
+	 *  
+	 * 
+	 * @return
+	 * @Example SELECT *: http://gw.systema.no:8080/syjservicesbcore/syjsSYHEDUMMY_URDR.do?user=OSCAR
+	 * @Example SELECT specific: http://gw.systema.no:8080/syjservicesbcore/syjsSYHEDUMMY_URDR.do?user=OSCAR&heavd=1
+	 * 
+	 */
+	
+	@RequestMapping(value="syjsSYHEDUMMY_URDR.do", method={RequestMethod.GET, RequestMethod.POST})
+	@ResponseBody
+	public String syjsRList_URD( HttpSession session, HttpServletRequest request) {
+		JsonResponseWriter jsonWriter = new JsonResponseWriter();
+		StringBuffer sb = new StringBuffer();
+		
+		try{
+			logger.info("Inside syjsSYHEDUMMY_URDR.do");
+			//TEST-->logger.info("Servlet root:" + AppConstants.VERSION_SYJSERVICES);
+			String user = request.getParameter("user");
+			
+			//Check ALWAYS user in BRIDF
+          String userName = this.bridfDaoServices.findNameById(user);
+          //DEBUG --> logger.info("USERNAME:" + userName + "XX");
+          String errMsg = "";
+			String status = "ok";
+			StringBuffer dbErrorStackTrace = new StringBuffer();
+			
+			//Start processing now
+			if(userName!=null && !"".equals(userName)){
+				//bind attributes is any
+				HedummyDao dao = new HedummyDao();
+				ServletRequestDataBinder binder = new ServletRequestDataBinder(dao);
+	            binder.bind(request);
+	            //At this point we now know if we are selecting a specific or all the db-table content (select *)
+	            List<IDao> list = null;
+				//-----------------------------
+	            //prepare service (mandatory)
+	            //-----------------------------
+	            this.hedummyDaoServices.setAlias("urd_alias");
+	            this.hedummyDaoServices.setMemberTable("hedummy");
+	            this.hedummyDaoServices.setMember("urd");
+	            
+	            //do SELECT
+				logger.info("Before SELECT ...");
+				if(dao.getHeavd() != null && !"".equals(dao.getHeavd())){
+					logger.info("Before findById ...");
+					list = this.hedummyDaoServices.findById(dao.getHeavd(), dbErrorStackTrace);
+					//logger.info("SIZE:" + list.size());
+				}else{
+					logger.info("Before getList ...");
+					list = this.hedummyDaoServices.getList(dbErrorStackTrace);
+					//logger.info("SIZE:" + list.size());
+				}
+				//process result
+				if (list!=null){
+					//write the final JSON output
+					sb.append(jsonWriter.setJsonResult_Common_GetList(userName, list));
+				}else{
+					//write JSON error output
+					errMsg = "ERROR on SELECT: list is NULL?  Try to check: <DaoServices>.getList";
+					status = "error";
+					logger.info("After SELECT:" + " " + status + errMsg );
+					sb.append(jsonWriter.setJsonSimpleErrorResult(userName, errMsg, status, dbErrorStackTrace));
+				}
+			}else{
+				//write JSON error output
+				errMsg = "ERROR on SELECT";
+				status = "error";
+				dbErrorStackTrace.append("request input parameters are invalid: <user>, <other mandatory fields>");
+				sb.append(jsonWriter.setJsonSimpleErrorResult(userName, errMsg, status, dbErrorStackTrace));
+			}
+
+		}catch(Exception e){
+			//write std.output error output
+			Writer writer = new StringWriter();
+			PrintWriter printWriter = new PrintWriter(writer);
+			e.printStackTrace(printWriter);
+			return "ERROR [JsonResponseOutputterController]" + writer.toString();
+		}
+		session.invalidate();
+		return sb.toString();
+	}
+	
 	
 	
 	//----------------
