d41c61bcd899e6075e0f65db059424941a587a0c
Fredrik Möller <fredrik@systema.no>
2019-04-05 13:25:37 +0200

    Added SVLTH and dep. model-mapper

---
+++ src/no/systema/jservices/bcore/z/maintenance/controller/BcoreMaintResponseOutputterController_SVLTH.java
@@ -0,0 +1,183 @@
+package no.systema.jservices.bcore.z.maintenance.controller;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.ServletRequestDataBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import no.systema.jservices.bcore.z.maintenance.controller.rules.SVLTH_U;
+import no.systema.jservices.common.dao.SvlthDao;
+import no.systema.jservices.common.dao.services.SvlthDaoService;
+import no.systema.jservices.common.json.JsonResponseWriter2;
+import no.systema.jservices.common.util.StringUtils;
+import no.systema.jservices.jsonwriter.JsonResponseWriter;
+import no.systema.jservices.model.dao.services.BridfDaoServices;
+
+@Controller
+public class BcoreMaintResponseOutputterController_SVLTH {
+	private static final Logger logger = Logger.getLogger(BcoreMaintResponseOutputterController_SVLTH.class.getName());
+
+	
+	/**
+	 * Search in SVLTH
+	 * 
+	 * Example :
+	 * http://localhost:8080/syjservicesbcore/syjsSVLTH?user=SYSTEMA&svlth_h=I&svlth_irn=1NO123456789012345
+	 */
+	@RequestMapping(path = "/syjsSVLTH", method = RequestMethod.GET)
+	@ResponseBody
+	public String syjsSVLTH(HttpSession session, 
+			@RequestParam(value = "user", required = true) String user,
+			@RequestParam(value = "svlth_h", required = false) String svlth_h,
+			@RequestParam(value = "svlth_irn", required = false) String svlth_irn,
+			@RequestParam(value = "svlth_id1", required = false) Integer svlth_id1,
+			@RequestParam(value = "svlth_id2", required = false) Integer svlth_id2,
+			@RequestParam(value = "DO_NOT_LOAD", required = false) String DO_NOT_LOAD) {
+
+		logger.info("INSIDE /syjsSVLTH...");
+		
+		JsonResponseWriter2<SvlthDao> jsonWriter = new JsonResponseWriter2<SvlthDao>();		
+		String errMsg = "";
+		String status = "ok";
+		StringBuffer dbErrorStackTrace = new StringBuffer();
+		StringBuffer sb = new StringBuffer();
+		List<SvlthDao> svlthDaoList = null;
+		
+		try {
+			String userName = bridfDaoServices.findNameById(user);
+			if (StringUtils.hasValue(userName)) {
+
+				if (DO_NOT_LOAD != null) {  //datatables trick
+					//do nothing
+					svlthDaoList = new ArrayList<SvlthDao>(); 
+				} else {
+					svlthDaoList = svlthDaoService.findAll(svlth_h,svlth_irn, svlth_id2 );
+				}
+
+				sb.append(jsonWriter.setJsonResult_Common_GetList(userName, svlthDaoList));
+				
+			}	else {
+				errMsg = "ERROR on SELECT";
+				status = "error";
+				dbErrorStackTrace.append("request input parameters are invalid: <user> ");
+				sb.append(jsonWriter.setJsonSimpleErrorResult(userName, errMsg, status, dbErrorStackTrace));
+			
+			}
+		} catch (Throwable e) {
+			logger.info("Error :", e);
+			Writer writer = new StringWriter();
+			PrintWriter printWriter = new PrintWriter(writer);
+			e.printStackTrace(printWriter);
+			return "ERROR [JsonResponseOutputterController]" + writer.toString();
+		}
+
+		session.invalidate();
+		return sb.toString();
+
+	}
+	
+	
+	@RequestMapping(value = "syjsSVLTH_U.do", method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public String syjsSVLTH_U(HttpSession session, HttpServletRequest request) {	
+		logger.info("INSIDE syjsSVLTH_U.do");
+		JsonResponseWriter jsonWriter = new JsonResponseWriter();
+		StringBuffer sb = new StringBuffer();
+		String userName = null;
+		String errMsg;
+		String status;
+		StringBuffer dbErrorStackTrace  = new StringBuffer();
+		String user = request.getParameter("user");
+		String mode = request.getParameter("mode");
+	
+		logger.info("user="+user);
+		logger.info("mode="+mode);
+		
+		
+		try {
+			// Check ALWAYS user in BRIDF
+			userName = this.bridfDaoServices.findNameById(user);
+			errMsg = "";
+			status = "ok";
+			SvlthDao dao = new SvlthDao();
+			SvlthDao resultDao = new SvlthDao();
+			ServletRequestDataBinder binder = new ServletRequestDataBinder(dao);
+			binder.bind(request);
+
+	        SVLTH_U rulerLord = new SVLTH_U(request, svlthDaoService, sb, dbErrorStackTrace); 
+	        
+			if (rulerLord.isValidInput(dao, userName)) {
+				if (StringUtils.hasValue(userName)) {
+						resultDao = svlthDaoService.create(dao);
+						logger.info("SVLTH created, dao="+ReflectionToStringBuilder.toString(resultDao));
+					if (resultDao == null) {
+						errMsg = "Could not add dao=" + ReflectionToStringBuilder.toString(dao);
+						status = "error ";
+						dbErrorStackTrace.append("Could not add dao=" + ReflectionToStringBuilder.toString(dao));
+						sb.append(jsonWriter.setJsonSimpleErrorResult(userName, errMsg, status, dbErrorStackTrace));
+					} else {
+						// OK UPDATE
+						sb.append(jsonWriter.setJsonSimpleValidResult(userName, status));
+					}
+				} else {
+					logger.error("user: "+user+ " not found in bridf.");
+					// write JSON error output
+					errMsg = "ERROR on DML for SVLTH";
+					status = "error";
+					dbErrorStackTrace.append("request input parameters are invalid: <user>");
+					sb.append(jsonWriter.setJsonSimpleErrorResult(userName, errMsg, status, dbErrorStackTrace));
+				}
+			} else {
+				// write JSON error output
+				errMsg = "ERROR on ADD for SVLTH: invalid rulerLord, error="+sb.toString();
+				status = "error";
+				sb.append(jsonWriter.setJsonSimpleErrorResult(userName, errMsg, status, dbErrorStackTrace));
+				logger.error(sb);
+			}
+			
+
+		} catch (Throwable e) {
+			logger.error("::ERROR::", e);
+			errMsg = "ERROR on add/update SVLTH: "+e.getMessage();
+			status = "error ";
+			dbErrorStackTrace.append(e.getMessage());
+			sb.append(jsonWriter.setJsonSimpleErrorResult(userName, errMsg, status,dbErrorStackTrace));
+		}
+		session.invalidate();
+		return sb.toString();
+		
+	}
+	
+	
+	@Qualifier ("bridfDaoServices")
+	private BridfDaoServices bridfDaoServices;
+	@Autowired
+	@Required
+	public void setBridfDaoServices (BridfDaoServices value){ this.bridfDaoServices = value; }
+	public BridfDaoServices getBridfDaoServices(){ return this.bridfDaoServices; }	
+
+	@Qualifier ("svlthDaoService")
+	private SvlthDaoService svlthDaoService;
+	@Autowired
+	@Required
+	public void setSvlthDaoService(SvlthDaoService value){ this.svlthDaoService = value; }
+	public SvlthDaoService getSvlthDaoService(){ return this.svlthDaoService; }		
+
+	
+}
---
+++ src/no/systema/jservices/bcore/z/maintenance/controller/rules/SVLTH_U.java
@@ -0,0 +1,43 @@
+package no.systema.jservices.bcore.z.maintenance.controller.rules;
+
+import javax.servlet.http.HttpServletRequest;
+
+import no.systema.jservices.common.dao.SvlthDao;
+import no.systema.jservices.common.dao.services.SvlthDaoService;
+import no.systema.jservices.common.values.EventTypeEnum;
+import no.systema.jservices.jsonwriter.JsonResponseWriter;
+import no.systema.main.util.MessageSourceHelper;
+/**
+ * 
+ * @author fredrikmoller
+ * @date 2019-04-01
+ */
+public class SVLTH_U {
+
+	private JsonResponseWriter jsonWriter = new JsonResponseWriter();
+	private MessageSourceHelper messageSourceHelper = null;
+	private StringBuffer errors = null;
+	private StringBuffer dbErrors = null;
+	private SvlthDaoService svlthDaoService = null;
+
+	
+	public SVLTH_U(HttpServletRequest request, SvlthDaoService svlthDaoService,StringBuffer sb, StringBuffer dbErrorStackTrace) {
+		messageSourceHelper = new MessageSourceHelper(request);
+		this.errors = sb;
+		this.dbErrors = dbErrorStackTrace;
+		this.svlthDaoService = svlthDaoService;
+	}
+	
+	public boolean isValidInput(SvlthDao dao, String user){
+		boolean retval = true;
+		if ( !svlthDaoService.exist(EventTypeEnum.INLAGG, dao.getSvlth_irn(), dao.getSvlth_id2())) {
+			errors.append(jsonWriter.setJsonSimpleErrorResult(user,
+					messageSourceHelper.getMessage("systema.bcore.accounting.error.exist", new Object[] { dao.getSvlth_irn(), dao.getSvlth_id2()}), "error", dbErrors));
+			retval = false;					
+		}	
+
+		return retval;
+
+	}
+
+}
