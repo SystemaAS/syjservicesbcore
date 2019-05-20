144971eb4999d0441a88043cfd7ed4e75ade992c
Fredrik Möller <fredrik@systema.no>
2019-05-20 11:16:46 +0200

    Added Svltf

---
+++ src/no/systema/jservices/bcore/z/maintenance/controller/BcoreMaintResponseOutputterController_SVLTF.java
@@ -0,0 +1,90 @@
+package no.systema.jservices.bcore.z.maintenance.controller;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import no.systema.jservices.common.dao.SvltfDao;
+import no.systema.jservices.common.dao.services.SvltfDaoService;
+import no.systema.jservices.common.json.JsonResponseWriter2;
+import no.systema.jservices.model.dao.services.BridfDaoServices;
+
+@Controller
+public class BcoreMaintResponseOutputterController_SVLTF {
+	private static final Logger logger = Logger.getLogger(BcoreMaintResponseOutputterController_SVLTF.class.getName());
+
+	@Autowired
+	private SvltfDaoService svltfDaoService;
+	
+	/**
+	 * @return
+	 * @Example SELECT specific: http://localhost:8080/syjservicesbcore/syjsSVLTF.do?user=FREDRIK
+	 * @Example SELECT list: http://localhost:8080/syjservicesbcore/syjsSVLTF.do?user=FREDRIK
+	 * 
+	 */
+	@RequestMapping(value="syjsSVLTF.do", method={RequestMethod.GET, RequestMethod.POST})
+	@ResponseBody
+	public String getSvltf(HttpSession session, HttpServletRequest request) {
+		JsonResponseWriter2<SvltfDao> jsonWriter = new JsonResponseWriter2<SvltfDao>();
+		StringBuffer sb = new StringBuffer();
+		List<SvltfDao> svltfDaoList = null;
+		
+		try {
+			String user = request.getParameter("user");
+			// Check ALWAYS user in BRIDF
+			String userName = this.bridfDaoServices.findNameById(user); 
+			String errMsg = "";
+			String status = "ok";
+			StringBuffer dbErrorStackTrace = new StringBuffer();
+
+			if ((userName != null && !"".equals(userName))) {
+				svltfDaoList = svltfDaoService.findAll(null);
+				if (svltfDaoList != null) {
+						sb.append(jsonWriter.setJsonResult_Common_GetList(userName, svltfDaoList));
+				} else {
+					errMsg = "ERROR on SELECT: Can not find SvltfDao list";
+					status = "error";
+					sb.append(jsonWriter.setJsonSimpleErrorResult(userName, errMsg, status, dbErrorStackTrace));
+				}
+
+			} else {
+				errMsg = "ERROR on SELECT";
+				status = "error";
+				dbErrorStackTrace.append("request input parameters are invalid: <user>");
+				sb.append(jsonWriter.setJsonSimpleErrorResult(userName, errMsg, status, dbErrorStackTrace));
+			}
+		} catch (Exception e) {
+			logger.info("Error :", e);
+			Writer writer = new StringWriter();
+			PrintWriter printWriter = new PrintWriter(writer);
+			e.printStackTrace(printWriter);
+			return "ERROR [JsonResponseOutputterController]" + writer.toString();
+		}
+
+		session.invalidate();
+		return sb.toString();
+
+	}
+
+	
+	@Qualifier ("bridfDaoServices")
+	private BridfDaoServices bridfDaoServices;
+	@Autowired
+	@Required
+	public void setBridfDaoServices (BridfDaoServices value){ this.bridfDaoServices = value; }
+	public BridfDaoServices getBridfDaoServices(){ return this.bridfDaoServices; }	
+
+}
